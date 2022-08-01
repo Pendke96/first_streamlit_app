e0047c9651023053ef60018f690c8dc1a1da46ed
Pendke96 <110379638+Pendke96@users.noreply.github.com>
2022-08-01 15:17:38 +0530

    Update streamlit_app.py
    Fixed spell check

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My Parents Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 and blueberry oatmeal')
-streamlit.text('kale, spinach and rocket smoothy')
-streamlit.text('hard boiled free-range egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
