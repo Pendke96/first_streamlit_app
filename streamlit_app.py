76322b35e0e12f13b167b67f2e921aa3ae1da882
Pendke96 <110379638+Pendke96@users.noreply.github.com>
2022-08-05 21:44:57 +0530

    making json data looking nicer

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalise json data 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output as table
+streamlit.dataframe(fruityvice_normalized)
